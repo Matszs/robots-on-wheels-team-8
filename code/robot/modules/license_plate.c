eff60e0917df09289fd989b56f44c71be94fc1c8
Suzanne Peerdeman <Suzanne.Peerdeman@hva.nl>
2015-12-07 12:55:52 +0100

    no message

--- code/robot/modules/license_plate.c
+++ code/robot/modules/license_plate.c
@@ -8,8 +8,14 @@
 #include  <stdio.h>
 #include  <unistd.h>
 #include  <time.h>
+#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 void *licensePlateHandler(void *arg);
+void check_database(char license[100]);
 
 void licensePlateReader() {
 	pthread_t licensePlateThread;
@@ -23,6 +29,7 @@ void *licensePlateHandler(void *arg) {
 	char buf[100];
 	while (fgets(buf, sizeof(buf), ls) != 0) {
 		printf("%s", buf);
+		check_database(buf);
 		writeToSocket(OPT_LICENSE, &buf[0]);
 	}
 
@@ -30,3 +37,55 @@ void *licensePlateHandler(void *arg) {
 }
 
 
+void finish_with_error(MYSQL *con)
+{
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);
+}
+
+void check_database(char license[100])
+{
+char query[300];
+
+sprintf(query,"SELECT Fine FROM Cars WHERE License = ('%s')",license);
+  MYSQL *con = mysql_init(NULL);
+
+  if (con == NULL)
+  {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      exit(1);
+  }
+
+  if (mysql_real_connect(con, "oege.ie.hva.nl", "peerdes001", "UXJPpVCuRfz.e/",
+          "zpeerdes001", 0, NULL, 0) == NULL)
+  {
+      finish_with_error(con);
+  }
+
+    if (mysql_query(con, query )) {
+      printf("Query failed: %s\n", mysql_error(con));
+    } else {
+      MYSQL_RES *result = mysql_store_result(con);
+
+      if (!result) {
+        printf("Couldn't get results set: %s\n", mysql_error(con));
+     }
+
+         int num_fields = mysql_num_fields(result);
+
+        MYSQL_ROW row;
+                 while ((row = mysql_fetch_row(result)))
+                {  
+                for(int i = 0; i < num_fields; i++)
+                {
+                     printf("%s ", row[i] ? row[i] : "NULL");
+                 }
+                         printf("\n");
+  }
+}
+
+  mysql_close(con);
+
+  return EXIT_SUCCESS;
+}
